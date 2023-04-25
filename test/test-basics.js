fcb9fa78b715471e52bd8de856d843bb4a643507
shreyas <shrs19cs@cmrit.ac.in>
2023-04-25 13:22:01 +0530

    modified ka

--- test/test-basics.js
+++ test/test-basics.js
@@ -4,15 +4,24 @@ http = require('http');
 var server = require('../app.js');
 
 describe('/', function () {
+
+  it('should return passed', function (done) {
+     var result=5*3;
+      assert.equal(result,15);
+      done();
+  });
+
   it('should return 200', function (done) {
-    http.get('http://localhost:8080', function (res) {
+    http.get('http://127.0.0.1:8080/', function (res) {
       assert.equal(200, res.statusCode);
       done();
     });
   });
 
+
+
   it('should return "Hello World!"', function (done) {
-    http.get('http://localhost:8080', function (res) {
+    http.get('http://127.0.0.1:8080/', function (res) {
       var data = '';
 
       res.on('data', function (chunk) {
